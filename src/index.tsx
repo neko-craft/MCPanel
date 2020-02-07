0080fd932518a84106bdeee2f94c0e5d8210b8a5
BBleae Sip <bbleae@baka.studio>
2020-02-07 15:12:46 +0800

    fix: SecurityError when using https

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,9 @@ const dom = document.getElementById('root')
 if (!('WebSocket' in window) || !('Proxy' in window) || !('fetch' in window)) {
   dom.innerHTML = '您的设备不支持显示本页面, 请更新至最新版的 Chrome 以获得最佳体验!'
 } else {
+  if (location.protocol === 'https:') {
+    location.protocol = 'http:'
+  }
   moment.locale('zh')
 
   ReactDOM.render(<ConfigProvider locale={zhCN}><App /></ConfigProvider>, dom)
