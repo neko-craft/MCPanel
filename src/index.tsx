cfc21b53a0da12d5f5e75f90439204bf6314d7e8
Shirasawa <764798966@qq.com>
2020-03-10 21:15:57 +0800

    edit notice

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import 'moment/locale/zh-cn'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -12,8 +13,8 @@ const dom = document.getElementById('root')
 if (!('WebSocket' in window) || !('Proxy' in window) || !('fetch' in window)) {
   dom.innerHTML = '您的设备不支持显示本页面, 请更新至最新版的 Chrome 以获得最佳体验!'
 } else {
-  if (window.location.protocol === 'https:') {
-    window.location.protocol = 'http:'
+  if (location.protocol === 'https:') {
+    location.protocol = 'http:'
   }
   moment.locale('zh')
 
